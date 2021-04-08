73d26d4c9de34837903530a039fc73b2ccd7a074
Nicklas <nifr@kea.dk>
2021-04-08 07:26:29 +0200

    Application Name

--- src/CarwashApplication.java
+++ src/CarwashApplication.java
@@ -1,7 +1,7 @@
 import washcard.WashCardList;
 import controller.Terminal;
 
-public class Main {
+public class CarwashApplication {
 
     public static void main(String[] args) {
         //Create test data: a list of wash card objects to be used for testing purpose
